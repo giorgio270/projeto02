965aa7382326286e2ef32103969ad4a2fd1641bb
ViniciusDeLucca <viniciusdelucca@hotmail.com>
2020-05-12 23:06:32 -0300

    Create Banco de Dados
    Criação do banco Users e Suppliers

---
+++ Projeto02Usuarios/src/java/projeto02/DbSuppliers.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto02;
+import java.util.ArrayList;
+/**
+ *
+ * @author Vinicius
+ */
+public class DbSuppliers {
+    private static ArrayList<Suppliers> supplier;
+    public static ArrayList<Suppliers> getSuppliers() {
+        if (supplier == null) {
+            Suppliers company = new Suppliers();
+            company.setAttributes("Company", "Commercial", "00000000000000", "00000000000", "Address, number 0000", "company@business.com");
+            supplier.add(company);
+        }
+        return supplier;
+    }
+}
---
+++ Projeto02Usuarios/src/java/projeto02/DbUsers.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto02;
+import java.util.ArrayList;
+/**
+ *
+ * @author Vinicius
+ */
+public class DbUsers {
+    private static ArrayList<Users> user;
+    public static ArrayList<Users> getUsers() {
+        if (user == null) {
+            Users person = new Users();
+            person.setAttributes("Person", "00000000000", "000000000", "00000000000", "Address, number 0000", "person@test.com");
+            user.add(person);
+        }
+        return user;
+    }
+}
